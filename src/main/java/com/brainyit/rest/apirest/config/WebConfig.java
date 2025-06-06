bde24abff3fdba3c6eac3bcd242e8cb6928d9c80
Ivan_cn <ivan.capeli@catijr.com.br>
2025-06-06 15:16:44 -0300

    feat: added global cors.

--- src/main/java/com/brainyit/rest/apirest/config/WebConfig.java
+++ src/main/java/com/brainyit/rest/apirest/config/WebConfig.java
@@ -1,13 +1,29 @@
 package com.brainyit.rest.apirest.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
+
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatterns = "";
+
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatterns.split(",");
+        registry.addMapping("/**")
+            .allowedOrigins(allowedOrigins)
+            //.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedMethods("*")
+            .allowCredentials(true);
+    }
+    
     @Override
     public void configureContentNegotiation(@NonNull ContentNegotiationConfigurer configurer) {
         
